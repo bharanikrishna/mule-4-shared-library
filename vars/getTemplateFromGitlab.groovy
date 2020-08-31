0d585f26f57d418ced3d3a07a442660530ffc376
M1053737 <54942737+M1053737@users.noreply.github.com>
2020-08-31 15:57:50 +0530

    Update getTemplateFromGitlab.groovy

--- vars/getTemplateFromGitlab.groovy
+++ vars/getTemplateFromGitlab.groovy
@@ -2,7 +2,7 @@ import com.mulesoft.Constants
 
 def call(String template) {
     deleteDir()
-    def url = "git@github.com:M1053737/rest-template.git"
+    def url = "https://github.com/M1053737/rest-template.git"
     
     git url: url // credentialsId: Constants.GITLAB_CREDENTIALS_ID,
     sh 'rm -r .git'
